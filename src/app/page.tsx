b629daa870d2453dfa403f3958134df8bdafcced
mibrahimzia <mibrahimzia44@gmail.com>
2025-06-22 23:31:53 +0500

    changes in chatbox, page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import ChatBox from '@/components/Chatbox'
+import Chatbox from '@/components/Chatbox'
 import Sidebar from '@/components/Sidebar'
 
 export default function HomePage() {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <ChatBox />
+      <Chatbox />
     </div>
   )
 }
